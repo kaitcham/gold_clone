7bdfdaf47e8e0054aa4e01bef81c45669b5b1451
kaitcham <chams4f@gmail.com>
2023-07-17 10:30:59 +0200

    Modify tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#3a3a3a',
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-      fontWeight: {
-        600: '600',
-      },
-      maxWidth: {
-        page: '1060px',
-      },
-      lineHeight: {
-        'extra-loose': '1.25',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
